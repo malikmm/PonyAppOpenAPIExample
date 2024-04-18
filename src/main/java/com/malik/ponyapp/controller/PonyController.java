de21efbf6e108787b3f019f73ed575b690aacc36
malikmm <malikmm@hotmail.com>
2024-04-18 17:03:56 +0100

    /pony/hello endpoint added

--- src/main/java/com/malik/ponyapp/controller/PonyController.java
+++ src/main/java/com/malik/ponyapp/controller/PonyController.java
@@ -3,14 +3,23 @@ package com.malik.ponyapp.controller;
 
 import com.malik.api.PonyApi;
 
+import com.malik.model.Hello;
 import com.malik.model.Pony;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PonyController implements PonyApi {
 
     @Override
     public ResponseEntity<Pony> getUsingGET() {
         return null;
+    }
+
+    @Override
+    public ResponseEntity<Hello> hello() {
+        return new ResponseEntity<>(new Hello().text("Hello World!"), HttpStatus.OK);
     }
 
     @Override
